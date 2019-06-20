716ed2d05258ede6514666718ad8ab5e71c1d08f
filipiakp <g12paerlfilipiak@gmail.com>
2019-06-20 16:08:14 +0200

    Initial

---
+++ src/main/java/com/filipiakp/warehousespring/WarehouseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.filipiakp.warehousespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseSpringApplication.class, args);
+	}
+
+}
